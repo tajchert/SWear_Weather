1ea4618d7c976d86d3639705e7d8d3d72e566d20
Michal Tajchert <mtajchert@gmail.com>
2015-08-20 12:15:04 +0200

    Crashlytics added.

---
+++ mobile/src/main/java/pl/tajchert/swear/SWearWeatherApp.java
@@ -0,0 +1,14 @@
+package pl.tajchert.swear;
+
+import android.app.Application;
+
+/**
+ * Created by Tajchert on 20.08.2015.
+ */
+public class SWearWeatherApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
